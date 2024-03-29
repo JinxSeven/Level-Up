89547a8f1331ecfd0cc1c7ebcbe977dd88434bdb
jinxseven <roadrustersam@gmail.com>
2024-03-30 02:00:43 +0530

    lets_go

--- Data_Structures/DS_doublyLL.cpp
+++ Data_Structures/DS_doublyLL.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 class Node {
+public:
     int data;
     Node *next;
     Node *prev;
@@ -14,5 +15,49 @@ class Node {
 };
 
 class DList {
-    
+public:
+    Node *head;
+    Node *tail;
+    int length {0};
+
+    DList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void appendList(int data) {
+        Node *newNode = new Node(data);
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    void viewList() {
+        Node *temp = head;
+        while (temp != nullptr) {
+            std::cout << temp->data <<std::endl;
+            temp = temp->next;
+        }
+        
+    }
+
+    void getLength() {
+        std::cout << "List Length: " << length <<std::endl;
+    }
 };
+
+int main() {
+    DList one;
+    for (int x = 10; x < 20; x++) {
+        one.appendList(x);
+    }
+
+    one.viewList();
+    one.getLength();
+}
