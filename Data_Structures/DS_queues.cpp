59a0f585e9cf0e0a0978ee67b49c2288c0e94a1b
jinxseven <roadrustersam@gmail.com>
2024-03-31 22:27:46 +0530

    queued_up

--- Data_Structures/DS_queues.cpp
+++ Data_Structures/DS_queues.cpp
@@ -16,7 +16,7 @@ class Queue {
 private:
     Node *first;
     Node *last;
-    unsigned int length;
+    unsigned int length {0};
 
 public:
     Queue() {
@@ -24,13 +24,66 @@ public:
         last = nullptr;
     }
 
+    bool isEmpty() {
+        return (length != 0) ? false : true;
+    }
+
     void enQueue(int data) {
         Node *newNode = new Node(data);
         if (first != nullptr) {
-
+            newNode->next = last;
+            last = newNode;
         } else {
             first = newNode;
             last = newNode;
         }
+        length++;
+    }
+
+    void deQueue() {
+        if (isEmpty()) {
+            std::cerr << "No elements found to dequeue \n";
+            return;
+        } else {
+            Node *temp = last;
+            while (temp->next != first) {
+                temp = temp->next;
+            }
+            // std::cout << temp->data;
+            delete first;
+            first = temp;
+        }
+        length--;
+    }
+
+    void viewQueue() {
+        if (isEmpty()) {
+            std::cout << "Queue is empty \n";
+            return;
+        } else {
+            Node *temp = last;
+            do {
+                std::cout << temp->data << std::endl;
+                temp = temp->next;
+            } while (temp != first->next);
+        }
+    }
+
+    void getLength() {
+        if (isEmpty()) {
+            std::cout << "Queue is empty \n";
+            return;
+        } else {
+            std::cout << "Queue length is: " << length << "\n";
+        }
     }
 };
+
+int main() {
+    Queue one;
+    for (int x = 0; x < 10; x++) {
+        one.enQueue(x);
+    }
+    one.deQueue();
+    one.viewQueue();
+}
