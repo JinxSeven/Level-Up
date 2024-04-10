dd876e0f4ca41486fea0a3e54755e6d433281229
Samuel Livingston <roadrustersam@gmail.com>
2024-04-10 23:06:24 +0530

    hash_reset

--- Data_Structures/DS_hash.cpp
+++ Data_Structures/DS_hash.cpp
@@ -13,30 +13,3 @@ class Node {
         next = nullptr;
     }
 };
-
-class Hash {
-    private:
-        static const int SIZE = 7;
-        Node *dataMaps[SIZE];
-
-    int hashIndex(std::string map) {
-        int hashx {0};
-        for (char x : map) {
-            hashx = (hashx + x) * 23;
-        }
-        return hashx % SIZE;
-    }
-
-    public:
-        void addHash(std::string map, int data) {
-            Node *new_node = new Node(map, data);
-            int index = hashIndex(map);
-            dataMaps[index] = new_node;
-        }
-
-};
-
-int main() {
-    Hash one;
-    one.addHash("beetroot", 545);
-}
