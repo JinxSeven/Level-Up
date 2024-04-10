fc61fe5423d95ae7744756cf649880a9128960bf
Samuel Livingston <roadrustersam@gmail.com>
2024-04-10 21:28:24 +0530

    clean

--- Data_Structures/DS_hash.cpp
+++ Data_Structures/DS_hash.cpp
@@ -31,33 +31,12 @@ class Hash {
         void addHash(std::string map, int data) {
             Node *new_node = new Node(map, data);
             int index = hashIndex(map);
-            if (dataMaps[index] != nullptr) {
-                Node *current = dataMaps[index];
-                while (current->next) {
-                    current = current->next;
-                }
-                current->next = new_node;
-            } else {
-                dataMaps[index] = new_node;
-            }
+            dataMaps[index] = new_node;
         }
 
-        int getHash(std::string map) {
-            int index = hashIndex(map);
-            Node *current = dataMaps[index];
-            while (map.compare(current->map)) {
-                current = current->next;
-            }
-            return current->data;
-        }
 };
 
 int main() {
     Hash one;
     one.addHash("beetroot", 545);
-    one.addHash("carrot", 445);
-    one.addHash("pumpkin", 345);
-
-    one.getHash("carrot");
-    
 }
