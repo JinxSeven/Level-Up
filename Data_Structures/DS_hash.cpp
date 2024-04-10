d98e44602b4bbd6c44d7f9884641a73c65bf902b
Samuel Livingston <john247samuel@gmail.com>
2024-04-10 15:51:43 +0530

    half_hash

--- Data_Structures/DS_hash.cpp
+++ Data_Structures/DS_hash.cpp
@@ -1,2 +1,49 @@
 #include <iostream>
+#include <string>
+
+class Node {
+    public:
+        std::string key;
+        int value;
+        Node *next;
+
+    Node(std::string key, int value) {
+        this->key = key;
+        this->value = value;
+        next = nullptr;
+    }
+};
+
+class Hash {
+    private:
+        static const int SIZE = 7;
+        Node *dataMaps[SIZE];
+
+    int hashIndex(std::string key) {
+        int hashx {0};
+        for (char x : key) {
+            hashx = (hashx + x) * 23;
+        }
+        return hashx % SIZE;
+    }
+
+    public:
+        void addHash(std::string key, int value) {
+            Node *new_node = new Node(key, value);
+            int index = hashIndex(key);
+            if (dataMaps[index] != nullptr) {
+                Node *current = dataMaps[index];
+                while (current) {
+                    current = current->next;
+                }
+                
+            } else {
+                dataMaps[index] = new_node;
+            }
+        }
+};
+
+int main() {
+    Hash one;
+}
 
