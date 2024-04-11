b932aa7075a58c3f11af5d47bf333f779b964504
Samuel Livingston <roadrustersam@gmail.com>
2024-04-11 10:33:26 +0530

    hash++

--- Data_Structures/DS_hash.cpp
+++ Data_Structures/DS_hash.cpp
@@ -13,3 +13,45 @@ class Node {
         next = nullptr;
     }
 };
+
+class Hash {
+    private:
+        static const int SIZE = 11;
+        Node *dataMaps[SIZE];
+
+    public:
+        Hash() {
+        for (int z = 0; z < SIZE; ++z) {
+            dataMaps[z] = nullptr;
+        }
+    }
+
+        void viewTable() {
+            for (int x = 0; x < SIZE; x++) {
+                std::cout << x << ": ";
+                if (dataMaps[x]) {
+                    Node *current = dataMaps[x];
+                    while (current) {
+                        std::cout << "{" << current->map << " - " << current->data << "}";
+                        current = current->next;
+                    }
+                    
+                }
+            }
+            std::cout << std::endl;
+        }
+
+        void indexHash(std::string map) {
+            for (char x : map) {
+                int hashx = x 
+            }
+        }
+
+        void addHash()
+};  
+
+int main() {
+    Hash one;
+    one.viewTable();
+    return 0;
+}
