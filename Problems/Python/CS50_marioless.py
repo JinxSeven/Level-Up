9d654d351b843dc57abd735eb30dac262ee951a7
Samuel Livingston <roadrustersam@gmail.com>
2024-04-12 06:18:53 +0530

    mario_less

--- Problems/Python/CS50_marioless.py
+++ Problems/Python/CS50_marioless.py
@@ -1,7 +1,9 @@
 while True:
-    height = int(input("Height: "))
-    if 0 < height < 9:
-        break
+    height = input("Height: ")
+    if height.isdigit():
+        height = int(height)
+        if 0 < height < 9:
+            break
 
 for x in range (height):
     for z in range (height):
