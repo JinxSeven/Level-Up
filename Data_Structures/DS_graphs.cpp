9ff4efcaa484671a91698e4b1fb08ac67e0fe84e
Samuel Livingston <john247samuel@gmail.com>
2024-04-16 12:13:05 +0530

    graphs_add

--- Data_Structures/DS_graphs.cpp
+++ Data_Structures/DS_graphs.cpp
@@ -1,2 +1,27 @@
 #include <iostream>
+#include <unordered_map>
+#include <unordered_set>
 
+class Graph {
+    private:
+        std::unordered_map<char, std::unordered_set<std::string>>adjList;
+
+    public:
+        bool addVertex(char vertex) {
+            if (adjList.count(vertex) == 0) {
+                adjList[vertex];
+                return true;
+            }
+            std::cout << "Duplicate Vertex\n";
+            return false;
+        }
+
+};
+
+int main() {
+    Graph *one = new Graph();
+
+    one->addVertex('A');
+    one->addVertex('B');
+    // one->addVertex('A');
+}
