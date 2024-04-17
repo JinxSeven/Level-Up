adf2475c64be33ed6d25f9f4dad3f5264e0eb039
Samuel Livingston <roadrustersam@gmail.com>
2024-04-18 02:27:02 +0530

    prime_ninety

---
+++ Prime/2D_arrays/90Deg.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#define SIZE 4
+char og_array[SIZE][SIZE];
+
+void ninetyDeg() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = i + 1; j < SIZE; j++) {
+            char temp = og_array[i][j];
+            og_array[i][j] = og_array[j][i];
+            og_array[j][i] = temp;
+        }
+    }
+
+    for (int i =0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE/2; j++) {
+            char temp = og_array[i][j];
+            og_array[i][j] = og_array[i][SIZE - 1 - j];
+            og_array[i][SIZE - 1 - j] = temp;
+        }
+    }
+}
+
+int main() {
+
+    char value = 'a';
+    for (int x = 0; x < (SIZE); x++) {
+        for (int z = 0; z < (SIZE); z++) {
+            og_array[x][z] = value++;
+        }
+    }
+
+    std::cout << "Before:\n"; 
+    for (int x = 0; x < (SIZE); x++) {
+        for (int z = 0; z < (SIZE); z++) {
+            std::cout << og_array[x][z] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    ninetyDeg();
+
+    std::cout << "After:\n"; 
+    for (int x = 0; x < (SIZE); x++) {
+        for (int z = 0; z < (SIZE); z++) {
+            std::cout << og_array[x][z] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
