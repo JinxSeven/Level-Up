3a0f43a24db20867f80d8b394cf8e749eae3da3a
Samuel Livingston <roadrustersam@gmail.com>
2024-04-18 10:49:40 +0530

    binary_search2D

--- Prime/2D_arrays/binary_search.cpp
+++ Prime/2D_arrays/binary_search.cpp
@@ -11,6 +11,30 @@ int main() {
         }
     }
     
-    int row = sizeof(matrix) / sizeof(matrix[0]);
+    int target;
+    std::cin >> target;
 
+    int row = sizeof(matrix) / sizeof(matrix[0]);
+    int col = sizeof(matrix[0]) / sizeof(matrix[0][0]);
+
+    int start = 0;
+    int end = (row * col) - 1; 
+    
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        int x = mid / col;
+        int z = mid % col;
+
+        if (matrix[x][z] > target) {
+            end = mid - 1;
+        }
+        else if (matrix[x][z] < target) {
+            start = mid + 1; 
+        }
+        else {
+            std::cout << "True\n";
+            return 0;
+        }
+    }
+    std::cout << "False\n";
 }
