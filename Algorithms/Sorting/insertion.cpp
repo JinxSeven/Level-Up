9d0bd3a116fd3193d71399ed8e8497dfe407b057
Samuel Livingston <john247samuel@gmail.com>
2024-04-25 10:28:50 +0530

    insertion_fixed

--- Algorithms/Sorting/insertion.cpp
+++ Algorithms/Sorting/insertion.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
-#include <utility>
 #include <vector>
 
-void insertions(std::vector<int> toSort) {
-    for (int w = 1; w < (toSort.size() - 1); w++) {
+void insertionSort(std::vector<int>& toSort) {
+    for (int w = 1; w < toSort.size(); w++) {
         int current = toSort[w];
         int s = w - 1;
-        while (current < toSort[s]) {
-            std::swap(toSort[w], toSort[s]);
-            if (s == 0) {
-                break;
-            }
+        while (s >= 0 && toSort[s] > current) {
+            toSort[s + 1] = toSort[s];
             s--;
         }
+        toSort[s + 1] = current;
     }
     for (auto a : toSort) {
         std::cout << a << std::endl;
     }
 }
 
-int main(int argc, char* argv[]) {
-    std::vector<int> unSort {1, 5, 9, 2, 0, 7};
-    insertions(unSort);
+int main() {
+    std::vector<int> arr = {5, 3, 8, 6, 2, 7, 1, 4};
+    insertionSort(arr);
+    return 0;
 }
