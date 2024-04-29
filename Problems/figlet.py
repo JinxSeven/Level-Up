3be295d7e4c443bd4b3045c318d737ad2a7119d7
Samuel Livingston <roadrustersam@gmail.com>
2024-04-29 13:13:31 +0530

    figlet

--- Problems/figlet.py
+++ Problems/figlet.py
@@ -1,8 +1,11 @@
 import sys
 import figlet
 
+argument_one = ["-f", "--font"]
+
 def main():
     if len(sys.argv) != 1 and len(sys.argv) != 3:
+        print("Invalid useage")
         sys.exit(1)
     
 main()
