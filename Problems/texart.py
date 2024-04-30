3e2193b1bf5d5117e0a297322a82a3876e905504
Samuel Livingston <john247samuel@gmail.com>
2024-04-30 20:13:51 +0530

    texart_updatedhelp

--- Problems/texart.py
+++ Problems/texart.py
@@ -3,15 +3,15 @@ import argparse
 from pyfiglet import Figlet
 
 def main():
-    arg = argparse.ArgumentParser(description="Generate ASCII Texart", epilog="Usage: python <scriptname>.py -f <fontname>")
-    arg.add_argument("-f", "--font", help=argparse.SUPPRESS)
-    argument = arg.parse_args()
+    parser = argparse.ArgumentParser(description="Generate cool ASCII art text using a font.")
+    parser.add_argument("-f", "--font", help="personal favorites: 'slant', 'lean', 'doh' http://www.figlet.org/examples.html", dest="fontname")
+    argument = parser.parse_args()
     
     if 1 < len(sys.argv) != 3:
         sys.stderr.write("Command-line arguments provided are invalid.")
         sys.exit(1)
     
-    font = argument.font if argument.font else "isometric3"
+    font = argument.fontname if argument.fontname else "poison"
     text = input("Text: ")
     output = Figlet(font=f'{font}')
     print(output.renderText(f'{text}'))
