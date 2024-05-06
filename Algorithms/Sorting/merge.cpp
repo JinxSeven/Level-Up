158819d19d7d7cef9ac5e4a55af2edb29c767da1
Samuel Livingston <john247samuel@gmail.com>
2024-05-06 14:40:08 +0530

    merge_updt

--- Algorithms/Sorting/merge.cpp
+++ Algorithms/Sorting/merge.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
-#include <vector>
 
-void merge(std::vector<int> unsorted) {
+void merge(int sort[], int mid, int lidx, int ridx) {
+    int lsize = (mid - lidx) + 1;
+}
 
+int main(int argc, char const *argv[])
+{
+    int unsort[] = {8, 7, 6, 5, 4, 3, 2, 1};
+    int size = sizeof(unsort) / sizeof(unsort[0]);
+    int mid = (size / 2) - 1;
+    int lidx = 0;
+    int ridx = size - 1;
+    merge(unsort, mid, lidx, ridx);
 }
