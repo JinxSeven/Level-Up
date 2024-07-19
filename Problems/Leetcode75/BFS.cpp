4a5210d853e4fd5452b68d49034ef6438674e056
Samuel Livingston <roadrustersam@gmail.com>
2024-07-19 21:46:23 +0530

    BFS_75

---
+++ Problems/Leetcode75/BFS.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+class Graph {
+private:
+    std::unordered_map<int, std::list<int>> grapher;
+
+public:
+    void addGraph(int vtx, std::list<int> edg) {
+        grapher[vtx] = edg;
+    }
+
+    void bfs(int src) {
+        std::unordered_map<int, bool> visited;
+        std::queue<int> bfsq;
+        
+        bfsq.push(src);
+        visited[src] = true;
+        while (!bfsq.empty()) {
+            auto x = bfsq.front();
+            bfsq.pop();
+
+            for (auto itr = grapher[x].begin(); itr != grapher[x].end(); itr++) {
+                if (visited.find(*itr) == visited.end()) {
+                    bfsq.push(*itr);
+                    visited[*itr] = true;
+                }
+            }
+            std::cout << x << "->";
+        }
+    }
+};
+
+int main(int argc, char** argv) {
+    Graph one;
+    one.addGraph(0, {1, 3});
+    one.addGraph(1, {0, 2});
+    one.addGraph(4, {5});
+    one.addGraph(5, {4});
+    one.addGraph(3, {0, 2, 4});
+    one.addGraph(2, {3, 1});
+
+    one.bfs(0);
+    std::cout << "end_of_graph" << std::endl;
+}
