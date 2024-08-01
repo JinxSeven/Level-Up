fe73e9f5b1a401d394ca65d1e2c6994945382f62
Samuel <roadrustersam@gmail.com>
2024-08-01 10:55:39 +0530

    ASPOUT

--- ASP101/Models/Entities.cs
+++
@@ -1,7 +0,0 @@
-namespace ASP101.Models {
-    public class Entities {
-        public int stud_id {set; get;}
-        public string stud_name {set; get;}
-        public string stud_course {set; get;}
-    }
-}
